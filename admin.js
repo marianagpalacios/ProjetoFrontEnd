7b31c496dc1b960a83f6c2206c666472d06c0d43
João Vitor <romanijoaovitor@gmail.com>
2025-11-18 16:04:08 -0300

    feat: captura os elementos que serão manipulados com JS

--- admin.js
+++ admin.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    const formCadastro = document.getElementById('formCadastro');
+    const nomeUsuarioInput = document.getElementById('nomeUsuario');
+    const emailUsuarioInput = document.getElementById('emailUsuario');
+    const listaUsuariosUL = document.getElementById('listaUsuarios');
+    const btnLimparCampos = document.getElementById('btnLimparCampos');
+    const btnLimparLista = document.getElementById('btnLimparLista');
+    const inputPesquisa = document.getElementById('inputPesquisa');
 });
